08a9add78e8e5a70f2401c572743c58b747e517a
Manuel Gainza Alvarez <gainza2014@gmail.com>
2022-12-17 18:38:36 -0300

    add contacBooks entity and relation with User

---
+++ AgendaApi/Entities/ContactsBook.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AgendaApi.Entities;
+
+public class ContactsBook
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = String.Empty;
+
+    public int Code { get; set; }
+
+    public ICollection<User> Users { get; set; }
+}
